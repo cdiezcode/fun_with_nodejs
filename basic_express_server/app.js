57c797147da68c66fb22841ab6e890cf081c400e
Ilya Danilov <peertosir@gmail.com>
2021-02-04 16:22:33 +0300

    Add styles and static files handler middleware

--- basic_express_server/app.js
+++ basic_express_server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const rootDir = require('./utils/path');
+
 //Routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -9,6 +11,7 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')))
 
 //Adding additional path for controller(routes)
 app.use('/admin', adminRoutes);
@@ -16,7 +19,7 @@ app.use(shopRoutes);
 
 //Handle not matched routes
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000, () => {
--- basic_express_server/routes/admin.js
+++ basic_express_server/routes/admin.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const path = require('path');
 
+const rootDir = require('../utils/path');
+
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
